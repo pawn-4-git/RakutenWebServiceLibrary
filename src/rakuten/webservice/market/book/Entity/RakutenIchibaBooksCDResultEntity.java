9eae2e5cbf10b0a997b4af7ae9f5834ddddca726
pawn4git <@192.168.1.100>
2017-05-14 19:27:34 +0900

    JavaDocの追加

--- src/rakuten/webservice/market/book/Entity/RakutenIchibaBooksCDResultEntity.java
+++ src/rakuten/webservice/market/book/Entity/RakutenIchibaBooksCDResultEntity.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
+ *楽天ブックのCDDVD検索の結果のクラス
  * @author Pawn
  */
 public class RakutenIchibaBooksCDResultEntity  extends RakutenIchibaBookResultEntity{
